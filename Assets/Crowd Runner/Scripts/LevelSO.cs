97dda2154b1101b1da91adabeccc1dee4b609eb1
Yosep Tara <tara@cakeplabs.com>
2023-01-19 10:49:14 +0800

    cleared excluded files

--- Assets/Crowd Runner/Scripts/LevelSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Level", menuName = "Scriptable Objects/Level", order = 0)]
-public class LevelSO : ScriptableObject
-{
-    public Chunk[] chunks;
-}
